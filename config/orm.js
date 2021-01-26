df400a0294970e728985e0c849df37b7a4b5a7c0
Zach <zdspringer99@gmail.com>
2021-01-25 22:05:23 -0800

    finished Building orm

--- config/orm.js
+++ config/orm.js
@@ -12,4 +12,66 @@ function printMarksOfQuestion(num) {
 
 function sqlObj(ob) {
   const arr = [];
+
+  for (const key in ob) {
+    const value = ob[key];
+    if (Object.hasOwnProperty.call(ob, key)) {
+      if (typeof value === "string" && value.indexOf(" ") >= 0) {
+        value = "'" + value + "'";
+      }
+      arr.push(key + "=" + value);
+    }
+  }
+  return arr.toString();
 }
+
+const orm = {
+  all: function (tableInput, cb) {
+    const queryString = "SELECT * FROM " + tableInput + ";";
+    connection.query(queryString, function (err, res) {
+      if (err) {
+        throw err;
+      }
+      cb(res);
+    });
+  },
+  create: function (table, cols, vals, cb) {
+    const queryString = "INSERT INTO" + table;
+
+    queryString += " (";
+    queryString += cols.toString();
+    queryString += ") ";
+    queryString += "VALUES (";
+    queryString += printMarksOfQuestion(vals.length);
+    queryString += ") ";
+
+    console.log(queryString);
+
+    connection.query(queryString, vals, function (err, res) {
+      if (err) {
+        throw err;
+      }
+
+      cb(res);
+    });
+  },
+};
+
+update: function(table, objColVals, condition, cb){
+    const queryString = "UPDATE " + table;
+
+    queryString += " SET ";
+    queryString += sqlObj(objColVals);
+    queryString += " WHERE ";
+    queryString += condition;
+
+    console.log(queryString);
+    connection.query(queryString,function(err, res){
+        if(err){
+            throw err;
+        }
+        cb(res);
+    });
+}
+
+module.export = orm;
